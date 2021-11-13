173988005537c0fa55bd10213189eb3df382643c
Wendy Grant <wendykgrant.tx@gmail.com>
2021-11-13 12:36:38 -0600

    deleted imgs and files

--- src/index.js
+++ src/index.js
@@ -31,24 +31,15 @@ import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
 import Sections from "views/Sections.js";
 import Coaches from "views/Coaches.js";
-// import Services from "views/Services";
-// import Presentation from "views/Presentation.js";
 import AboutUs from "views/examples/AboutUs.js";
-import AddProduct from "views/examples/AddProduct.js";
 import CoachesPersonal from "./views/CoachesPersonal";
 import CoachesProfessional from "./views/CoachesProfessional";
 import ContactUs from "views/examples/ContactUs.js";
-import Discover from "views/examples/Discover.js";
-import Error404 from "views/examples/Error404.js";
-import Error422 from "views/examples/Error422.js";
-import Error500 from "views/examples/Error500.js";
 import Home from "views/examples/Home.js";
 import Services from "views/examples/Services.js";
 import LoginPage from "views/examples/LoginPage.js";
-import ProductPage from "views/examples/ProductPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 import RegisterPage from "views/examples/RegisterPage.js";
-import SearchWithSidebar from "views/examples/SearchWithSidebar.js";
 import Settings from "views/examples/Settings.js";
 import Membership from "views/examples/Membership";
 
@@ -65,9 +56,6 @@ ReactDOM.render(
       
       <Route path="/sections" render={(props) => <Sections {...props} />} />
       
-      {/* <Route
-        path="/presentation"
-        render={(props) => <Presentation {...props} />} /> */}
 
       <Route path="/about-us" render={(props) => <AboutUs {...props} />} />
       
@@ -75,21 +63,9 @@ ReactDOM.render(
 
       <Route path="/membership" render={(props) => <Membership {...props} />} />
 
-      <Route
-        path="/add-product"
-        render={(props) => <AddProduct {...props} />}
-      />
       
       <Route path="/contact-us" render={(props) => <ContactUs {...props} />} />
-      
-      <Route path="/discover" render={(props) => <Discover {...props} />} />
-      
-      <Route path="/error-404" render={(props) => <Error404 {...props} />} />
-      
-      <Route path="/error-422" render={(props) => <Error422 {...props} />} />
-      
-      <Route path="/error-500" render={(props) => <Error500 {...props} />} />
-      
+            
       <Route path="/coaches" render={(props) => <Coaches {...props} />} />
       
       <Route path="/coaches-professional" render={(props) => <CoachesProfessional {...props} />} />
@@ -100,10 +76,6 @@ ReactDOM.render(
       
       <Route path="/services" render={(props) => <Services {...props} />} />
       
-      <Route
-        path="/product-page"
-        render={(props) => <ProductPage {...props} />}
-      />
       
       <Route
         path="/profile-page"
@@ -115,10 +87,6 @@ ReactDOM.render(
         render={(props) => <RegisterPage {...props} />}
       />
       
-      <Route
-        path="/search-with-sidebar"
-        render={(props) => <SearchWithSidebar {...props} />}
-      />
       
       <Route path="/settings" render={(props) => <Settings {...props} />} />
       <Redirect to="/index" />
